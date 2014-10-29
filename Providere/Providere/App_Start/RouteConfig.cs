40beeaab38df120bd67ba2fe6385c335af7c78cc
CeGisele <cegisele@hotmail.com>
2014-10-28 23:04:00 -0300

    Agregando funcionalidades a Ingresar y Registrarse

--- Providere/Providere/App_Start/RouteConfig.cs
+++ Providere/Providere/App_Start/RouteConfig.cs
@@ -13,6 +13,10 @@ namespace Providere
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // BotDetect requests must not be routed
+            routes.IgnoreRoute("{*botdetect}",
+              new { botdetect = @"(.*)BotDetectCaptcha\.ashx" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Providere/Providere/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Providere.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        //
+        // GET: /Usuario/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        public ActionResult Ingresar()
+        {
+            return View();
+        }
+
+    }
+}
