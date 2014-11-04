d470e09a477de52befa9ea93b681a2c94405982e
CeGisele <cegisele@hotmail.com>
2014-11-04 15:57:38 -0300

    Sentencias para modificar campos en tabla

---
+++ SQLServer/Modificaciones.sql
@@ -0,0 +1,4 @@
+/*Sentencias para modificar campos en tablas:*/
+
+alter table dbo.Usuario alter column Telefono varchar(20) not null
+alter table dbo.Usuario alter column Contrasenia varchar(50) not null
