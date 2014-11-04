cc18edb935cc758cb75bc4c121f96e22fc760193
CeGisele <cegisele@hotmail.com>
2014-11-04 01:34:07 -0300

    Agregando clases especiales para encriptar y enviar mail

---
+++ Providere/Providere/Servicios/Encryptor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Providere.Servicios
+{
+    public static class Encryptor
+    {
+        public static string MD5Hash(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
+        }
+    }
+}
