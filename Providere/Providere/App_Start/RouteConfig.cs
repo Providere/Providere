2cfe3554d1873cef4b96c310afdb2782b27a8ac1
CeGisele <cegisele@hotmail.com>
2014-11-10 20:21:16 -0300

    modificacion

--- Providere/Providere/App_Start/RouteConfig.cs
+++ Providere/Providere/App_Start/RouteConfig.cs
@@ -26,7 +26,7 @@ namespace Providere
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
