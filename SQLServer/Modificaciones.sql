ae3503ba9470c5015f593332ed9417c9b85cfe12
CeGisele <cegisele@hotmail.com>
2014-11-13 23:31:35 -0300

    Insercion de rubros

--- SQLServer/Modificaciones.sql
+++ SQLServer/Modificaciones.sql
@@ -1,4 +1,6 @@
 /*Sentencias para modificar campos en tablas:*/
+USE [Providere]
+GO
 
 alter table dbo.Usuario alter column Telefono varchar(20) not null
 alter table dbo.Usuario alter column Contrasenia varchar(50) not null
