c1ac6a3b0b50d6cbf3edd2187477bab16aff4dfc
CeGisele <cegisele@hotmail.com>
2014-11-13 22:47:06 -0300

    Agregando Publicacion

---
+++ Providere/Providere/Servicios/PublicacionServicios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Providere.Models;
+using Providere.Repositorios;
+
+namespace Providere.Servicios
+{
+    public class PublicacionServicios
+    {
+       
+    }
+}
