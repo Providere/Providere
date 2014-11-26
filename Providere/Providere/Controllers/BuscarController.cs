d4d97a28a3eba1085b874d55d9ebee3516074e89
AgoFig <agostinafigueredo@gmail.com>
2014-11-26 17:00:24 -0300

    Agregado Buscador controller, service y view

---
+++ Providere/Providere/Controllers/BuscarController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Providere.Servicios;
+using Providere.Models;
+
+namespace Providere.Controllers
+{
+    public class BuscarController : Controller
+    {
+        //
+        // GET: /Buscar/Resultados
+
+        BuscarServicios ps = new BuscarServicios();
+        ProvidereEntities context = new ProvidereEntities();
+
+        public ActionResult Resultados(Int16 Rubro, Int16 SubRubro, String Ubicacion)
+        {
+            
+            var publicaciones = ps.buscar(Rubro,SubRubro,Ubicacion); 
+            return View(publicaciones.ToList());
+        }
+
+    }
+}
---
+++ Providere/Providere/Servicios/BuscarServicios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Providere.Repositorios;
+using Providere.Models;
+
+namespace Providere.Servicios
+{
+    public class BuscarServicios
+    {
+        PublicacionRepositorio pr = new PublicacionRepositorio();
+
+        internal List<Publicacion> buscar(short Rubro, short SubRubro, string Ubicacion)
+        {
+            return pr.buscarPorRubroSubRubroUbicacion(Rubro,SubRubro,Ubicacion);
+            throw new NotImplementedException();
+        }
+    }
+}
