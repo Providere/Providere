f50712e98abf6362742ad8245ca63bf12b1dfa9b
AgoFig <agostinafigueredo@gmail.com>
2015-02-12 18:00:25 -0300

    Ya se peude recortar la foto de perfil

---
+++ Providere/Providere/Controllers/ImagenController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Providere.Servicios;
+
+namespace Providere.Controllers
+{
+    public class ImagenController : Controller
+    {
+
+        [HttpPost]
+        public virtual ActionResult RecortarImagen(string imagePath, int? cropPointX, int? cropPointY, int? imageCropWidth, int? imageCropHeight)
+        {
+            if (string.IsNullOrEmpty(imagePath) || !cropPointX.HasValue || !cropPointY.HasValue || !imageCropWidth.HasValue || !imageCropHeight.HasValue)
+            {
+                return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest);
+            }
+
+            byte[] imageBytes = System.IO.File.ReadAllBytes(Server.MapPath(imagePath));
+            byte[] croppedImage = ImageHelper.CropImage(imageBytes, cropPointX.Value, cropPointY.Value, imageCropWidth.Value, imageCropHeight.Value);
+
+            string tempFolderName = Server.MapPath("~/Imagenes/FotoPerfil/" + ConfigurationManager.AppSettings["Image.TempFolderName"]);
+            string fileName = Path.GetFileName(imagePath);
+
+            try
+            {
+                FileHelper.SaveFile(croppedImage, Path.Combine(tempFolderName, fileName));
+            }
+            catch (Exception ex)
+            {
+                //Log an error     
+                return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
+            }
+
+            string photoPath = string.Concat("/Imagenes/FotoPerfil/", ConfigurationManager.AppSettings["Image.TempFolderName"], "/", fileName);
+            return Json(new { photoPath = photoPath }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
