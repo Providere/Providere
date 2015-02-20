2cde9621eb4f292f118455fbbb778f7233939731
agofig <agostinafigueredo@gmail.com>
2015-02-19 23:54:37 -0300

    Corregidos tama;os de botones de habilitar deshabilitar para que nose corten en resoluciones peque;as

--- Providere/Providere/Content/js/script.js
+++ Providere/Providere/Content/js/script.js
@@ -63,6 +63,16 @@ $(function() {
 
 
 
-	});
+	
+    });
+
+
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 150) {
+        $('.header').addClass('shrink');
+    } else {
+        $('.header').removeClass('shrink');
+    }
+});
 
 });
