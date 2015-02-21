44a5daf372ac9bdfe81ea2c36fe7ac555beda6ce
Paula Montero <pau.montero88@gmail.com>
2015-02-21 15:43:53 -0300

    Tenia conflicto

--- Providere/Providere/Servicios/PuntajeServicios.cs
+++ Providere/Providere/Servicios/PuntajeServicios.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -23,6 +23,9 @@ namespace Providere.Servicios
         }
 
 
-
+        public List<Puntaje> TraerPuntajeUsuariosCercanos(List<Publicacion> publicacionesUsuariosCercanos)
+        {
+            return pur.TraerPuntajeUsuariosCercano(publicacionesUsuariosCercanos);
+        }
     }
 }
