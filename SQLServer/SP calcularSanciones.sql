e1b3a65dbe382aadc3464cc836f5a125e1cf98d4
agofig <agostinafigueredo@gmail.com>
2015-02-23 11:55:28 -0300

    Creados SP para sancion

--- SQLServer/SP calcularSanciones.sql
+++ SQLServer/SP calcularSanciones.sql
@@ -1,27 +1,29 @@
 Use Providere;
 
-CREATE PROCEDURE calcularSanciones
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SP_calcularSanciones') AND type in (N'P', N'PC'))
+  DROP PROCEDURE SP_calcularSanciones
+GO
+create PROCEDURE SP_calcularSanciones
 AS
+BEGIN
 
-UPDATE FROM Usuario SET Estado = 3 
-WHERE idUsuario IN (SELECT idUsuario FROM Denuncia WHERE Fecha > DATEADD(month, -1, GETDATE()))
-AND idUsuario IN (
-Select idUsuario WHERE AVG(Shoesize)
+UPDATE Usuario SET IdEstado = 3 
+WHERE Id IN (
+  Select   
+  c.idcalificado
+  FROM calificacion as c
+  INNER JOIN denuncia as d
+  ON c.id = d.idCalificacion
+  WHERE 
+  c.idTipoEvaluacion = 3 AND
+  c.fechaCalificacion > DATEADD(month, -1, GETDATE())
+   group by c.idcalificado
+  HAVING 
+  (Select Count(idTipoEvaluacion) as cantNegativas From calificacion as df where c.idcalificado = df.idcalificado  aND idTipoEvaluacion = 3 group by df.idcalificado) > 5 
+  AND 
+  (Count(idTipoEvaluacion)* 100 / (Select Count(*) From calificacion as cc where c.idcalificado = cc.idcalificado aND idTipoEvaluacion = 3  group by cc.idcalificado)) >= 60
+  )
 
-)
-AND idUsuario IN (SELECT Count(id) FROM Calificacion AS c WHERE c.IdTipoEvaluacion = 3, Count(denuncia.id) > 1 AND c.IdUsuario =idUsuario)
-
-
-
- List<Calificacion> calificacionesNegativas = cs.obtenerNegativasDeUsuario(usuario);
-
-                int porcentajeDeDenuncias = (calificacionesNegativas.Count * 100) / usuario.Calificacion.Count;
-
-                if (porcentajeDeDenuncias > 90)
-                {
-                    establecerSancion(usuario);
-                }
-
-
-
+END
 GO
